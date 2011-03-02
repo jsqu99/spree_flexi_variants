a08024811fba9ab34d62a992144c2317da27a6eb
Jeff Squires <jeff.squires@gmail.com>
2011-03-02 11:36:28 -0500

    fix references

--- db/migrate/20110301182123_create_product_option_values.rb
+++ db/migrate/20110301182123_create_product_option_values.rb
@@ -1,10 +1,9 @@
 class CreateProductOptionValues < ActiveRecord::Migration
   def self.up
     create_table :product_option_values do |t|
-      t.references :product
-      t.references :option_value
+      t.integer  "product_id"
+      t.integer  "option_value_id"
       t.decimal :price_modifier, :null => true, :default => nil, :precision => 8, :scale => 2
-
       t.timestamps
     end
   end

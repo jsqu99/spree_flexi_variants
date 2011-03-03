d267df2eecb188981228c206a73119a2205782df
Jeff Squires <jeff.squires@gmail.com>
2011-03-03 09:17:00 -0500

    temporarily comment out hook for admin/option_values

--- lib/spree_on_demand_variants_hooks.rb
+++ lib/spree_on_demand_variants_hooks.rb
@@ -1,4 +1,4 @@
 class SpreeOnDemandVariantsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_product_form_additional_fields, 'admin/products/has_on_demand_variants'
-  insert_after :admin_product_tabs, 'admin/products/admin_option_values'
+#  insert_after :admin_product_tabs, 'admin/products/admin_option_values'
 end

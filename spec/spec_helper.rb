f367d7690adefda873c805a17c9e7c231aafebd4
Jeff Squires <jeff.squires@gmail.com>
2011-05-23 13:37:42 -0400

    change paths around.  to run tests, simply hand-create a new rails app, update the Gemfile, and copy over this extension's spec directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -14,9 +13,10 @@ Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each do |f|
   require fp
 end
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -28,13 +28,11 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  #config.include Devise::TestHelpers, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
+  # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
 end
-
-@configuration ||= AppConfiguration.find_or_create_by_name("Default configuration")

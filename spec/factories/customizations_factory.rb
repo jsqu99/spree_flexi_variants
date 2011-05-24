69d135986b734d4eb7ecb4991402d99782ee2b2b
Jeff Squires <jeff.squires@gmail.com>
2011-05-24 17:09:30 -0400

    first 'good-looking', 'correct' RSpec test & accompanying factories.  All previous checkins were CRAP as I stumbled through the FactoryGirl/RSpec noobyland learning curve

--- spec/factories/customizations_factory.rb
+++ spec/factories/customizations_factory.rb
@@ -3,7 +3,6 @@ Factory.define :product_customization_type do |f|
   f.presentation  { Faker::Lorem.words }
   f.description  {  Faker::Lorem.words }
   f.products []
-  f.association :calculator
 end
 
 Factory.define :customizable_product_option do |f|
@@ -13,6 +12,7 @@ Factory.define :customizable_product_option do |f|
   f.data_validation { ({:type => :string, :required => false}).to_json }
   f.association :product_customization_type
 end
+
 
 Factory.define :product_customization do |f|
   f.association :product_customization_type
@@ -25,3 +25,32 @@ Factory.define :customized_product_option do |f|
   f.association :customizable_product_option
 end
 
+
+# now for the engraving customizations
+
+Factory.define :customizable_inscription, :parent => :customizable_product_option do |f|
+  f.name { "inscription" }
+  f.association :product_customization_type, :factory => :engraving_customization_type
+end
+
+
+Factory.define :customized_inscription, :parent => :customized_product_option do |f|
+  f.value { "to my best friend" }
+  f.association :product_customization # , :factory => :engraving_customization
+  f.association :customizable_product_option, :factory => :customizable_inscription
+end
+
+Factory.define :engraving_calculator, :class => Calculator::Engraving do |f|
+  f.after_create do |c|
+    c.set_preference(:price_per_letter, 10.0)
+  end
+end
+
+Factory.define :engraving_customization_type, :parent => :product_customization_type do |f|
+  f.after_create {|cust_type| cust_type.calculator = Factory(:engraving_calculator,:calculable =>  cust_type) }
+end
+
+Factory.define :engraving_customization, :parent => :product_customization do |f|
+  f.association :product_customization_type, :factory => :engraving_customization_type
+  f.customized_product_options { [ Factory(:customized_inscription) ]}
+end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,116 +2,72 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Order do
 
-  let(:order) { Factory(:order2) }
+  let(:order) { Factory(:order_with_totals) }  # has a line_item attached
   let(:product) { Factory(:product) }
+  let(:ad_hoc_option_values) {               # a list of two ad_hoc_option_values
+    [
+      Factory(:ad_hoc_option_value),
+      Factory(:ad_hoc_option_value)
+    ]
+  }
 
-  context "#add ad hoc product" do
+  context "#add product with ad hoc options selected" do
+
     before do
-
-      # TODO: so I KNOW this is some ugly-ass shit that needs to moved into the
-      # factories...I'm working on my mad rspec/factory_girl skillz...so bear with me
-      option_types=[]
-      @ahovs=[]
-
-      # create at least two option types
-      (rand(4)+2).times { option_types << Factory(:option_type_with_multiple_values) }
-
-      # create one ad hoc option type for each option type
-      option_types.each do |ot|
-        ahot = Factory(:ad_hoc_option_type, :product => product, :option_type => ot)
-
-        # create the n ad hoc option values that correspond to the n option_values in the ot
-        ot.option_values.each do |ov|
-          @ahovs << Factory(:ad_hoc_option_value, :ad_hoc_option_type => ahot, :option_value => ov)
-        end
-      end
-
-      @ov_ids = @ahovs.map(&:id).map(&:to_s)
+      @ov_ids = ad_hoc_option_values.map(&:id).map(&:to_s)
     end
 
-    context "with new configuration" do
-      it "should return new line item" do
-        orig_items = order.line_items
-        new_items = orig_items.clone
-        li= order.add_variant(product.master, @ov_ids, [], 1) # the [] is the guaranteed non-nil customizations.  See module ProductCustomizations for more info
+    context "with a new option combination" do
+      it "returns a new line item" do
+        orig_items = order.line_items.clone
+
+        li= order.add_variant(product.master, @ov_ids, [], 1)
         li.should_not be_nil
-        new_items.include?(li).should be_false
+        orig_items.include?(li).should be_false
       end
     end
 
-    context "with existing configuration" do
-      let(:existing_line_item) { Factory.build(:line_item, :order=>order, :variant => product.master) }
+    context "with an option combination identical to that of one already added to cart" do
+      it "returns the existing line item with the quantity incremented" do
+        orig_li = order.line_items[0]
+        orig_li.variant_id = product.master.id # ensure we are attempting to add the same variant
+        orig_li.ad_hoc_option_values = ad_hoc_option_values
 
-      before do
-        @ahovs.each do |a|
-          existing_line_item.ad_hoc_option_values << a
-        end
-        existing_line_item.product_customizations=[]
-        existing_line_item.save
-      end
+        orig_qty = orig_li.quantity
+        li= order.add_variant(product.master, @ov_ids, [], 1)
 
-      it "should return existing line item with quantity incremented" do
-        orig_qty = existing_line_item.quantity
-        li= order.add_variant(product.master, @ov_ids, [], 1) # the [] is the guaranteed non-nil customizations.  See module ProductCustomizations for more info
-
-        li.id.should == existing_line_item.id
+        li.id.should ==  order.line_items[0].id
         li.quantity.should == orig_qty+1
       end
     end
   end
 
-  context "#add customizable product" do
-    let(:calculator) { Calculator::ProductArea.new }
+  context "#add a product having a customization" do
 
-    before do
-      calculator.stub :preferred_min_width => 4
-      calculator.stub :preferred_max_width => 40
-      calculator.stub :preferred_min_height => 5
-      calculator.stub :preferred_max_height => 50
-      calculator.stub :preferred_min_pricing_area => 20
-      calculator.stub :preferred_multiplier => 3
+    let (:customization) { Factory(:engraving_customization) }
 
-      # TODO: so I KNOW this is some ugly-ass shit that needs to moved into the
-      # factories...I'm working on my mad rspec/factory_girl skillz...so bear with me
+    context "with a unique customization" do
+      it "returns a new line item" do
+        orig_items = order.line_items.clone
 
-      @cust_type = Factory(:product_customization_type, :calculator => calculator, :products => [product])
-      @height_opt_type = Factory(:customizable_product_option, :name => 'height', :product_customization_type => @cust_type,:data_validation => ({:type => :decimal, :required => true}).to_json)
-      @width_opt_type = Factory(:customizable_product_option, :name => 'width',  :product_customization_type => @cust_type,:data_validation => ({:type => :decimal, :required => true}).to_json)
-
-      @customization = Factory(:product_customization, :product_customization_type => @cust_type)
-      @height_cust =  Factory(:customized_product_option, :value => 10, :product_customization => @customization, :customizable_product_option => @height_opt_type)
-      @width_cust = Factory(:customized_product_option,   :value => 10, :product_customization => @customization, :customizable_product_option => @width_opt_type)
-    end
-
-    context "with new configuration" do
-      it "should return new line item" do
-        orig_items = order.line_items
-        new_items = orig_items.clone
-        li= order.add_variant(product.master, [], [@customization], 1)
+        li= order.add_variant(product.master, [], [customization], 1)
         li.should_not be_nil
-        new_items.include?(li).should be_false
+        orig_items.include?(li).should be_false
       end
     end
 
-#    context "with existing configuration" do
-#
-#      before do
-#        @new_customization = Factory(:product_customization, :product_customization_type => @cust_type)
-#        @new_height_cust =  Factory(:customized_product_option, :value => 10, :product_customization => @new_customization, :customizable_product_option => @height_opt_type)
-#        @new_width_cust = Factory(:customized_product_option,   :value => 10, :product_customization => @new_customization, :customizable_product_option => @width_opt_type)
-#
-#        @existing_line_item = Factory(:line_item2, :order=>order, :variant => product.master, :product_customizations => [@new_customization])
-#
-#        # let's add the 'same' configuration down below (without the foreign key attached)
-#        @customization.line_item = @existing_line_item
-#      end
-#
-#      it "should return existing line item" do
-#        orig_qty = @existing_line_item.quantity
-#        li= order.add_variant(product.master, [], [@new_customization], 1)
-#        li.id.should == @existing_line_item.id
-#        li.quantity.should == orig_qty+1
-#      end
-#    end
+    context "with a customization identical to that of one already added to cart" do
+      it "returns existing line item" do
+        orig_li = order.line_items[0]
+        orig_li.variant_id = product.master.id # ensure we are attempting to add the same variant
+        orig_li.product_customizations = [customization]
+
+        orig_qty = order.line_items[0].quantity
+
+        li= order.add_variant(product.master, [], [customization], 1)
+        li.id.should == order.line_items[0].id
+        li.quantity.should == orig_qty+1
+      end
+    end # context
   end # context
 end # add customizable product

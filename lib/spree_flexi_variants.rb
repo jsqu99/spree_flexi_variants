c3a081bbb72e8472ae951ee3ffd37421b64c2473
Jeff Squires <jeff.squires@gmail.com>
2011-10-17 18:01:27 -0400

    new railtie calculator registration mechanism

--- lib/spree_flexi_variants.rb
+++ lib/spree_flexi_variants.rb
@@ -11,7 +11,14 @@ module SpreeFlexiVariants
         Rails.env.production? ? require(c) : load(c)
       end
 
-      [Calculator::Engraving, Calculator::AmountTimesConstant, Calculator::ProductArea, Calculator::CustomizationImage].each(&:register)
+      initializer "spree.register.calculators" do |app|
+        app.config.spree.calculators.product_customization_types = [
+          Calculator::Engraving,
+          Calculator::AmountTimesConstant,
+          Calculator::ProductArea,
+          Calculator::CustomizationImage
+        ]
+      end
     end
 
     config.to_prepare &method(:activate).to_proc

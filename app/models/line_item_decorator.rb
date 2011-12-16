2748262caf95a8d6fb777f3b98eab1b76e7a2c9e
Jeff Squires <jeff.squires@gmail.com>
2011-12-16 18:10:15 -0500

    add a bunch of dependent => destroy

--- app/models/line_item_decorator.rb
+++ app/models/line_item_decorator.rb
@@ -1,5 +1,5 @@
 LineItem.class_eval do
-  has_many :ad_hoc_option_values_line_items
+  has_many :ad_hoc_option_values_line_items, :dependent => :destroy
   has_many :ad_hoc_option_values, :through => :ad_hoc_option_values_line_items
-  has_many :product_customizations 
+  has_many :product_customizations, :dependent => :destroy
 end
--- app/models/product_customization_type.rb
+++ app/models/product_customization_type.rb
@@ -1,9 +1,6 @@
 class ProductCustomizationType < ActiveRecord::Base
   calculated_adjustments  # does this just need to be changed to has_one :calculator?
   has_and_belongs_to_many :products
-  has_many :customizable_product_options
+  has_many :customizable_product_options, :dependent => :destroy
   accepts_nested_attributes_for :customizable_product_options, :allow_destroy => true
-
-  # make it prettier to get the only calculator
-
 end

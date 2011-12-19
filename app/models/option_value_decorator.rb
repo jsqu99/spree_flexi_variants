3cdd160626418c9a6fe9c108e2618df23ef384e0
Jeff Squires <jeff.squires@gmail.com>
2011-12-19 14:58:57 -0500

    add better dependent => destroy behavior

--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -1,4 +1,3 @@
 OptionValue.class_eval do
-  has_one :ad_hoc_option_value, :dependent => :destroy
-  has_many :excluded_ad_hoc_option_values, :dependent => :destroy
+  has_many :ad_hoc_option_values, :dependent => :destroy
 end

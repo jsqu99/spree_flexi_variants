7b5a73f11a189afa4e18c4fb3ddff43550ea1684
Jeff Squires <jeff.squires@gmail.com>
2012-03-03 18:44:13 -0500

    git rid of price modifier in cart description.  use DL vs. OL for ad hoc options

--- app/overrides/replace_line_item_row.rb
+++ app/overrides/replace_line_item_row.rb
@@ -4,22 +4,3 @@ Deface::Override.new(:virtual_path => "spree/shared/_order_details",
                      :partial => "spree/shared/order_details_line_item_row",
                      :disabled => false)
 
-Deface::Override.new(:virtual_path => "spree/orders/_line_item",
-                     :name => "converted_line_item_price_1234",
-                     :replace => "[data-hook='cart_item_price']",
-                     :text => %q(
-                       <td>
-                         <%= number_to_currency line_item.price %>
-                       </td>
-                     ))
-
-Deface::Override.new(:virtual_path => "spree/orders/_line_item",
-                     :name => "converted_line_item_total_456",
-                     :replace => "[data-hook='cart_item_total']",
-                     :text => %q(
-                       <td>
-                         <%= number_to_currency (line_item.price * line_item.quantity) %>
-                       </td>
-                     ))
-
-

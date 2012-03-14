15c6acef2439855e4634ca3917e8a0baefffff63
Jeff Squires <jeff.squires@gmail.com>
2012-03-13 20:25:36 -0400

    fix override path to use spree namespace

--- app/overrides/add_admin_tab_for_product_customization_types.rb
+++ app/overrides/add_admin_tab_for_product_customization_types.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_sub_menu",
                      :name => "converted_admin_product_sub_tabs_203014347",
                      :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
                      :text => %{<%= tab :product_customization_types, :match_path => '/product_customization_types'},

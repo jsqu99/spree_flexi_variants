59eabf617fdb0e7a203b0b15482c3949821cdbd9
Matthias Wagner <matthias.wagner@online.de>
2012-03-29 19:30:35 +0200

    deface Issue #26 removed :disabled => false

--- app/overrides/add_cart_item_description.rb
+++ app/overrides/add_cart_item_description.rb
@@ -1,6 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/orders/_line_item",
                          :name => "converted_cart_item_description_722158932",
                          :insert_bottom => "[data-hook='cart_item_description'], #cart_item_description[data-hook]",
-                         :partial => "spree/orders/cart_item_description",
-                         :disabled => false)
-
+                         :partial => "spree/orders/cart_item_description")

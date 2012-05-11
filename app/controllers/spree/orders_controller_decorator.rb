2d354df6a664d6ddf7af90134467936827112054
Jeff Squires <jeff.squires@gmail.com>
2012-05-11 11:49:59 -0400

    changes the parameter order for add_variant.  the intent was to nicely coexist w/ existing extensions and spree core itself by having the first two parameters match spree.  this switch makes that true

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -15,12 +15,12 @@ module Spree
       params[:products].each do |product_id,variant_id|
         quantity = params[:quantity].to_i if !params[:quantity].is_a?(Hash)
         quantity = params[:quantity][variant_id].to_i if params[:quantity].is_a?(Hash)
-        @order.add_variant(Variant.find(variant_id), ad_hoc_option_value_ids, product_customizations, quantity) if quantity > 0
+        @order.add_variant(Variant.find(variant_id), quantity, ad_hoc_option_value_ids, product_customizations) if quantity > 0
       end if params[:products]
 
       params[:variants].each do |variant_id, quantity|
         quantity = quantity.to_i
-        @order.add_variant(Variant.find(variant_id), ad_hoc_option_value_ids, product_customizations, quantity) if quantity > 0
+        @order.add_variant(Variant.find(variant_id), quantity, ad_hoc_option_value_ids, product_customizations) if quantity > 0
       end if params[:variants]
 
       redirect_to cart_path

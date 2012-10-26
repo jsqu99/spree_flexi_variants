43d9754f1a8fd04168b0be4e7f6e6397b890212b
Jeff Squires <jeff.squires@gmail.com>
2012-10-26 08:49:49 -0400

    respects position of option_values

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -16,6 +16,7 @@ module Spree
       ad_hoc_option_type.option_type.option_values.each do |ov|
         ahot = AdHocOptionValue.new()
         ahot.option_value_id = ov.id
+        ahot.position = ov.position
         ahot.save
         ad_hoc_option_type.ad_hoc_option_values << ahot
       end

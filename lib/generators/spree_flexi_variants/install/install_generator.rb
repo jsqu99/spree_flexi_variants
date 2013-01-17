731968bde1deab019312743b27d495c01d154309
Jerrold Thompson <jet@whidbey.com>
2013-01-17 09:07:07 -0800

    Remove injection of css and jscript from install_generator.rb

--- lib/generators/spree_flexi_variants/install/install_generator.rb
+++ lib/generators/spree_flexi_variants/install/install_generator.rb
@@ -2,13 +2,13 @@ module SpreeFlexiVariants
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_flexi_variants\n"
-      end
+      # def add_javascripts
+      #   append_file "app/assets/javascripts/store/all.js", "//= require store/spree_flexi_variants\\n"
+      # end
 
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree-flexi-variants\n", :before => /\*\//, :verbose => true
-      end
+      # def add_stylesheets
+      #   inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree-flexi-variants\\n", :before => /\\*\\//, :verbose => true
+      # end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_flexi_variants'

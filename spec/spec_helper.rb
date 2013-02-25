3971b4a737876356b4b632753f2d7f69d6ecef5d
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-02-25 15:19:24 -0500

    Use MiniMagick instead of RMagick.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 
+require 'ffaker'
+
 #include spree's factories
 require 'spree_core'
 

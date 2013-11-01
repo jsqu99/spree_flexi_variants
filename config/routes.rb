0148b005ba478ceef92c21206edb82e28686a0bb
charredUtensil <cjd.d01071@gmail.com>
2013-11-01 15:34:02 -0400

    use correct function to draw routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.append do
+Spree::Core::Engine.routes.add_routes do
   match 'product_customizations/price', :to => 'product_customizations#price', :via => [:get, :post]
 
   match 'customize/:product_id', :to => 'products#customize', :as => 'customize', :via => [:get, :post]

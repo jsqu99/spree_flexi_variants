221e3fd21a619c4594096422dd2a4c31e0cb8828
Jeff Squires <jeff.squires@gmail.com>
2013-11-20 02:21:31 -0500

    adds testing improvements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,8 @@ require 'spree/testing_support/capybara_ext'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/url_helpers'
 
+require 'spree_flexi_variants/factories'
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include IntegrationHelpers

b4d1f186f5685294b85e87b132e96a817eabbfc2
Jeff Squires <jeff.squires@gmail.com>
2013-11-20 22:25:28 -0500

    made some possibly-unecessary changes to the way the calculators are set up

--- app/models/spree/calculator/customization_image.rb
+++ app/models/spree/calculator/customization_image.rb
@@ -1,3 +1,5 @@
+require_dependency 'spree/calculator'
+
 module Spree
   class Calculator::CustomizationImage < Calculator
     preference :price, :decimal
--- app/models/spree/calculator/no_charge.rb
+++ app/models/spree/calculator/no_charge.rb
@@ -1,3 +1,5 @@
+require_dependency 'spree/calculator'
+
 module Spree
   class Calculator::NoCharge < Calculator
     def self.description

195987bc9fcafd75563942dfea4504f98d537166
Seb Ashton <seb@madebymade.co.uk>
2014-03-10 13:41:44 +0000

    specify join table

--- app/models/spree/product_customization_type.rb
+++ app/models/spree/product_customization_type.rb
@@ -3,7 +3,7 @@ module Spree
     include Spree::Core::CalculatedAdjustments
     self.table_name = "spree_product_customization_types"
 
-    has_and_belongs_to_many :products
+    has_and_belongs_to_many :products, join_table: "spree_product_customization_types_products"
 
     has_many :customizable_product_options, :dependent => :destroy
     accepts_nested_attributes_for :customizable_product_options, :allow_destroy => true

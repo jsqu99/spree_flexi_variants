1ae7db22ce60c729084392cd5bb9ccd99c6eec12
Seb Ashton <seb@madebymade.co.uk>
2014-03-10 12:13:17 +0000

    making atta accessible

--- app/models/spree/product_customization_type.rb
+++ app/models/spree/product_customization_type.rb
@@ -1,11 +1,12 @@
 module Spree
   class ProductCustomizationType < ActiveRecord::Base
+    include Spree::Core::CalculatedAdjustments
     self.table_name = "spree_product_customization_types"
 
-    include Spree::Core::CalculatedAdjustments
+    attr_accessible :name, :presentation, :description, :customizable_product_options_attributes
+    accepts_nested_attributes_for :customizable_product_options, :allow_destroy => true
+
     has_and_belongs_to_many :products
     has_many :customizable_product_options, :dependent => :destroy
-    accepts_nested_attributes_for :customizable_product_options, :allow_destroy => true
-    #attr_accessible :name, :presentation, :description, :customizable_product_options_attributes
   end
 end

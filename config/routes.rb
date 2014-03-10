52ba5957c13cd38347372372234dd3c875e340b1
Seb Ashton <seb@madebymade.co.uk>
2014-03-10 11:14:21 +0000

    prepend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.prepend do
   match 'product_customizations/price', :to => 'product_customizations#price', :via => [:get, :post]
 
   match 'customize/:product_id', :to => 'products#customize', :as => 'customize', :via => [:get, :post]

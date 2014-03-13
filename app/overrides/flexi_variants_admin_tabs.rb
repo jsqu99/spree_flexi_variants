7e4ad5492ccaf111b0b047e2a8bb4ac348ad84fd
Seb Ashton <seb@madebymade.co.uk>
2014-03-13 21:02:25 +0000

    remove the tab from main admin nav

--- app/overrides/flexi_variants_admin_tabs.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "flexi_variants_admin_tabs",
-                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:product_customization_types, :url => spree.admin_product_customization_types_path) %>")
